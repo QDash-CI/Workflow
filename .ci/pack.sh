44ae7c4bb6f4dc5166680acfbcf1a36b417e0ee7
crueter <crueter@eden-emu.dev>
2025-09-13 01:35:55 -0400

    typo
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/pack.sh
+++ .ci/pack.sh
@@ -15,7 +15,7 @@ done
 for arch in amd64 arm64
 do
   cp windows-$arch/*.zip artifacts/QDash-Windows-${ID}-${arch}.zip
-  cp windows-setup-$arch.exe artifacts/QDash-Windows-${ID}-${arch}.exe
+  cp windows-setup-$arch/*.exe artifacts/QDash-Windows-${ID}-${arch}.exe
 done
 
 cp macos-universal/*.tar.gz artifacts/QDash-macOS-${ID}.tar.gz
