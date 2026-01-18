2862ef2b1291799c6130e0dbbe5495810e0e61e6
crueter <crueter@eden-emu.dev>
2026-01-18 14:48:07 -0500

    try bundling mesa-zink
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/linux/deps.sh
+++ .ci/linux/deps.sh
@@ -38,4 +38,4 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-mesa --prefer-nano qt6-base-mini libxml2-mini opus-mini ffmpeg-mini
+./get-debloated-pkgs.sh --add-mesa --prefer-nano qt6-base-mini libxml2-mini opus-mini ffmpeg-mini mesa-zink
