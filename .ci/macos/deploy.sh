62048685e782a51b977118bb5b8438ab01acd39d
crueter <crueter@eden-emu.dev>
2026-03-11 15:56:22 -0400

    Update deploy.sh

--- .ci/macos/deploy.sh
+++ .ci/macos/deploy.sh
@@ -5,32 +5,67 @@
 APP="$1"
 EXE="$2"
 
+[ $# -ge 2 ] || { echo "usage: deploy.sh /path/to/app ExecutableName"; exit 1; }
+
 _frameworks="$APP"/Contents/Frameworks
 mkdir -p "$_frameworks"
 
-[ $# -ge 2 ] || { echo "usage: deploy.sh /path/to/app ExecutableName"; exit 1; }
-
 # Exclude system frameworks and libraries
 libs() {
-	grep -v "/usr/lib" | grep -v "/System/Library/Frameworks" | tr -d '\t' | cut -d' ' -f1 | grep dylib
+grep -v "/usr/lib" | grep -v "/System/Library/Frameworks" | tr -d '\t' | cut -d' ' -f1 | grep dylib | grep -v ':' | grep -v 'executable_path' || :
 }
 
 deploy() {
-	_app="$1"
-	LIBS="$(otool -L "$_app" | libs)"
+_app="$1"
+LIBS="$(otool -L "$_app" | libs)"
 
-	# TODO(crueter): Handle Frameworks and such
-	for lib in $LIBS; do
-		[ ! -f "$_frameworks/$lib" ] || continue
+    echo "-- Walking $_app"
 
-		_name="${lib##*/}"
-		_path="@executable_path/../Frameworks/$_name"
-		install_name_tool -id "$_path" "$_app"
+# TODO(crueter): Handle Frameworks and such
+    # walk thru deps...
+for lib in $LIBS; do
+        echo "-- * $lib"
 
-		cp "$lib" "$_frameworks/$_name"
+        # ignore otool's stupid id rules
+_name="${lib##*/}"
+        _appname="${_app##*/}"
+        if [ "$_name" = "$_appname" ]; then
+            continue
+        fi
 
-		deploy "$lib"
-	done
+        # then reset the dylib's path
+_path="@executable_path/../Frameworks/$_name"
+install_name_tool -change "$lib" "$_path" "$_app" 2>/dev/null
+
+[ -f "$_frameworks/$_name" ] || cp "$lib" "$_frameworks/$_name"
+
+        # and change its internal structure
+        install_name_tool -id "$_path" "$_frameworks/$_name" 2>/dev/null
+done
+
+    for lib in $LIBS; do
+_name="${lib##*/}"
+        _appname="${_app##*/}"
+        if [ "$_name" = "$_appname" ]; then
+            continue
+        fi
+
+        deploy "$_frameworks/$_name"
+    done
 }
 
 deploy "$APP/Contents/MacOS/$EXE"
+
+# Now strip if llvm is available
+if brew --prefix llvm >/dev/null 2>&1; then
+    echo "-- Stripping $APP data"
+
+    _strip="$(brew --prefix llvm)/bin/llvm-strip"
+    find "$APP" -type f | while read -r file; do
+        set +e
+        "${_strip}" "${file}" 2>/dev/null && echo "-- Stripped $file"
+        set -e
+    done
+fi
+
+codesign --deep --force --verbose --sign - "$APP"
