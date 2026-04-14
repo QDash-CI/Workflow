754fe21f72b21cafe9bec294dc42fcba1caff617
crueter <crueter@eden-emu.dev>
2026-04-13 20:46:55 -0400

    set -x
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/macos/deploy.sh
+++ .ci/macos/deploy.sh
@@ -5,67 +5,70 @@
 APP="$1"
 EXE="$2"
 
-[ $# -ge 2 ] || { echo "usage: deploy.sh /path/to/app ExecutableName"; exit 1; }
+[ $# -ge 2 ] || {
+	echo "usage: deploy.sh /path/to/app ExecutableName"
+	exit 1
+}
 
 _frameworks="$APP"/Contents/Frameworks
 mkdir -p "$_frameworks"
 
 # Exclude system frameworks and libraries
 libs() {
-grep -v "/usr/lib" | grep -v "/System/Library/Frameworks" | tr -d '\t' | cut -d' ' -f1 | grep dylib | grep -v ':' | grep -v 'executable_path' || :
+	grep -v "/usr/lib" | grep -v "/System/Library/Frameworks" | tr -d '\t' | cut -d' ' -f1 | grep dylib | grep -v ':' | grep -v 'executable_path' || :
 }
 
 deploy() {
-_app="$1"
-LIBS="$(otool -L "$_app" | libs)"
+	_app="$1"
+	LIBS="$(otool -L "$_app" | libs)"
 
-    echo "-- Walking $_app"
+	echo "-- Walking $_app"
 
-# TODO(crueter): Handle Frameworks and such
-    # walk thru deps...
-for lib in $LIBS; do
-        echo "-- * $lib"
+	# TODO(crueter): Handle Frameworks and such
+	# walk thru deps...
+	for lib in $LIBS; do
+		echo "-- * $lib"
 
-        # ignore otool's stupid id rules
-_name="${lib##*/}"
-        _appname="${_app##*/}"
-        if [ "$_name" = "$_appname" ]; then
-            continue
-        fi
+		# ignore otool's stupid id rules
+		_name="${lib##*/}"
+		_appname="${_app##*/}"
+		if [ "$_name" = "$_appname" ]; then
+			continue
+		fi
 
-        # then reset the dylib's path
-_path="@executable_path/../Frameworks/$_name"
-install_name_tool -change "$lib" "$_path" "$_app" 2>/dev/null
+		# then reset the dylib's path
+		_path="@executable_path/../Frameworks/$_name"
+		install_name_tool -change "$lib" "$_path" "$_app" 2>/dev/null
 
-[ -f "$_frameworks/$_name" ] || cp "$lib" "$_frameworks/$_name"
+		[ -f "$_frameworks/$_name" ] || cp "$lib" "$_frameworks/$_name"
 
-        # and change its internal structure
-        install_name_tool -id "$_path" "$_frameworks/$_name" 2>/dev/null
-done
+		# and change its internal structure
+		install_name_tool -id "$_path" "$_frameworks/$_name" 2>/dev/null
+	done
 
-    for lib in $LIBS; do
-_name="${lib##*/}"
-        _appname="${_app##*/}"
-        if [ "$_name" = "$_appname" ]; then
-            continue
-        fi
+	for lib in $LIBS; do
+		_name="${lib##*/}"
+		_appname="${_app##*/}"
+		if [ "$_name" = "$_appname" ]; then
+			continue
+		fi
 
-        deploy "$_frameworks/$_name"
-    done
+		deploy "$_frameworks/$_name"
+	done
 }
 
 deploy "$APP/Contents/MacOS/$EXE"
 
 # Now strip if llvm is available
 if brew --prefix llvm >/dev/null 2>&1; then
-    echo "-- Stripping $APP data"
+	echo "-- Stripping $APP data"
 
-    _strip="$(brew --prefix llvm)/bin/llvm-strip"
-    find "$APP" -type f | while read -r file; do
-        set +e
-        "${_strip}" "${file}" 2>/dev/null && echo "-- Stripped $file"
-        set -e
-    done
+	_strip="$(brew --prefix llvm)/bin/llvm-strip"
+	find "$APP" -type f | while read -r file; do
+		set +e
+		"${_strip}" "${file}" 2>/dev/null && echo "-- Stripped $file"
+		set -e
+	done
 fi
 
 codesign --deep --force --verbose --sign - "$APP"
