8867016e6207fd1f56a19d6d565e4c5070776f5e
crueter <crueter@eden-emu.dev>
2026-05-27 03:22:55 -0400

    Oops
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/common/pack.sh
+++ .ci/common/pack.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-# SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+# SPDX-FileCopyrightText: Copyright 2026 crueter
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # shellcheck disable=SC2043
