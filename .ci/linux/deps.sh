c2e416b33f7ef723b64c572da0839ddcc3ebfca4
crueter <crueter@eden-emu.dev>
2026-06-04 17:07:09 -0400

    uegh
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/linux/deps.sh
+++ .ci/linux/deps.sh
@@ -55,9 +55,4 @@ tar xf qt.tar.zst -C /usr
 
 # oopsies
 rm -rf /usr/lib/qt6/qml/QtCharts /usr/lib/*Charts* /usr/lib/cmake/Qt6Qml/QmlPlugins/*charts*
-rm -rf /usr/lib/qt6/qml/Qt/labs /usr/lib/*Labs* /usr/lib/cmake/*Labs*
-
-# ugh
-for lab in labs Labs folderlistmodel settings wavefront sharedimage; do
-	rm -rf /usr/lib/cmake/Qt6Qml/QmlPlugins/*${lab}*
-done
+rm -rf /usr/lib/qt6/qml/Qt/labs/platform /usr/lib/*LabsPlatform* /usr/lib/cmake/Qt6Qml/QmlPlugins/*Platform*
